300d3e4114d9bf5c6356426faab7092829c6c66d
victorlistapad <victorlistapad@gmail.com>
2024-11-05 23:42:07 -0500

    New: - Created logic to add chosen item to the order - Created logic to render order Change: - Removed HTML placeholders for add items

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 import menuArray from "./data.js";
+const orderArr = [];
+document.addEventListener("click", (event) => {
+  if (event.target.dataset.addItemId) {
+    addItemToOrder(+event.target.dataset.addItemId);
+    document.querySelector(`.checkout-container`).classList.remove("hidden");
+  }
+});
 
 function renderMenu(arr) {
   const menuSectionContainer = document.querySelector(`#menu-items`);
@@ -13,10 +20,38 @@ function renderMenu(arr) {
                       )}</p>
                       <h4 class="item-price">$${item.price}</h4>
                     </div>
-                    <button class="item-add-btn">+</button>
+                    <button class="item-add-btn" data-add-item-id="${
+                      item.id
+                    }">+</button>
                   </div>`;
   });
   menuSectionContainer.innerHTML += htmlToAdd;
 }
-
 renderMenu(menuArray);
+
+// ADD ITEM TO ORDER
+function addItemToOrder(id) {
+  const foundItem = menuArray.find((el) => el.id === id);
+  orderArr.push(foundItem);
+  renderOrder();
+}
+
+// RENDER ORDER
+function renderOrder() {
+  let checkoutContainer = document.querySelector(`.checkout-items-container`);
+  let htmlToAdd = "";
+  orderArr.forEach(
+    (el) =>
+      (htmlToAdd += `
+          <div class="single-checkout-item">
+          <h3 class="item-name">${el.name}</h3>
+          <button class="remove-btn">remove</button>
+          <h4 class="item-price">$${el.price}</h4>
+          </div>`)
+  );
+  checkoutContainer.innerHTML = htmlToAdd;
+  document.querySelector(`.total-price`).textContent = `$${orderArr.reduce(
+    (acc, cur) => acc + cur.price,
+    0
+  )}`;
+}
