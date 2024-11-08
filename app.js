1e7ec52ef2259f607e3612f884591b0420d96565
victorlistapad <victorlistapad@gmail.com>
2024-11-07 23:42:05 -0500

    New: - Added logic to stre chosen items in local storage - Added logic to render order if order was stored in the local storage - Added ;ogic to open and close checkout confirmation screen

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
 import menuArray from "./data.js";
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 const orderArr = [];
+// localStorage.clear();
+checkLS();
 document.addEventListener("click", (event) => {
   if (event.target.dataset.addItemId) {
     addItemToOrder(+event.target.dataset.addItemId);
-    document.querySelector(`.checkout-container`).classList.remove("hidden");
+  } else if (event.target.dataset.removeItemId) {
+    removeItemFromOrder(event.target.dataset.removeItemId);
+  } else if (event.target.id === "checkout-btn") {
+    showConfirmationScreen();
+  } else if (event.target.id === "close-confirmation-screen") {
+    closeConfirmationScreen();
   }
 });
 
+// RENDER MENU
 function renderMenu(arr) {
   const menuSectionContainer = document.querySelector(`#menu-items`);
   let htmlToAdd = "";
   arr.forEach((item) => {
     htmlToAdd += `<div class="single-item">
-                    <div class="item-icon-container">${item.emoji}</div>
-                    <div class="item-info">
-                      <h3 class="item-name">${item.name}</h3>
-                      <p class="item-ingredients">${item.ingredients.join(
-                        ", "
-                      )}</p>
-                      <h4 class="item-price">$${item.price}</h4>
-                    </div>
-                    <button class="item-add-btn" data-add-item-id="${
-                      item.id
-                    }">+</button>
-                  </div>`;
+    <div class="item-icon-container">${item.emoji}</div>
+    <div class="item-info">
+    <h3 class="item-name">${item.name}</h3>
+    <p class="item-ingredients">${item.ingredients.join(", ")}</p>
+    <h4 class="item-price">$${item.price}</h4>
+    </div>
+    <button class="item-add-btn" data-add-item-id="${item.id}">+</button>
+    </div>`;
   });
   menuSectionContainer.innerHTML += htmlToAdd;
 }
@@ -32,26 +37,73 @@ renderMenu(menuArray);
 // ADD ITEM TO ORDER
 function addItemToOrder(id) {
   const foundItem = menuArray.find((el) => el.id === id);
-  orderArr.push(foundItem);
+  orderArr.push({ ...foundItem, orderId: uuidv4() });
+  // ADD ITEMS TO THE LOCAL STORAGE
+  localStorage.setItem(`myOrder`, JSON.stringify(orderArr));
+  console.log(orderArr);
+  renderOrder();
+}
+
+// REMOVE ITEM FROM ORDER
+function removeItemFromOrder(id) {
+  orderArr.push(
+    ...orderArr.splice(0, orderArr.length).filter((item) => item.orderId !== id)
+  );
+  localStorage.setItem(`myOrder`, JSON.stringify(orderArr));
   renderOrder();
 }
 
 // RENDER ORDER
 function renderOrder() {
+  if (orderArr.length < 1) {
+    document.querySelector(`.checkout-container`).classList.add("hidden");
+  } else if (orderArr.length >= 1) {
+    document.querySelector(`.checkout-container`).classList.remove("hidden");
+  }
   let checkoutContainer = document.querySelector(`.checkout-items-container`);
   let htmlToAdd = "";
   orderArr.forEach(
     (el) =>
       (htmlToAdd += `
-          <div class="single-checkout-item">
-          <h3 class="item-name">${el.name}</h3>
-          <button class="remove-btn">remove</button>
-          <h4 class="item-price">$${el.price}</h4>
-          </div>`)
+        <div class="single-checkout-item">
+        <h3 class="item-name">${el.name}</h3>
+        <button class="remove-btn" data-remove-item-id="${el.orderId}">remove</button>
+        <h4 class="item-price">$${el.price}</h4>
+        </div>`)
   );
   checkoutContainer.innerHTML = htmlToAdd;
   document.querySelector(`.total-price`).textContent = `$${orderArr.reduce(
     (acc, cur) => acc + cur.price,
     0
   )}`;
+}
+
+// CHECK LOCAL STORAGE FOR ORDER
+function checkLS() {
+  if (localStorage.getItem(`myOrder`)) {
+    orderArr.push(...JSON.parse(localStorage.getItem(`myOrder`)));
+    renderOrder();
+  }
+}
+
+// SHOW ORDER CONFIRMATION SCREEN
+function showConfirmationScreen() {
+  document
+    .querySelector(`.checkout-confirmation-screen`)
+    .classList.remove("hidden");
+  document.querySelector(`main`).classList.add(`disabled`);
+  checkAndConfirm();
+}
+
+// CLOSE ORDER CONFIRMATION SCREEN
+function closeConfirmationScreen() {
+  document
+    .querySelector(`.checkout-confirmation-screen`)
+    .classList.add("hidden");
+  document.querySelector(`main`).classList.remove(`disabled`);
+}
+
+// CHECK AND CONFIRM ORDER
+function checkAndConfirm() {
+  const inputVal = document.querySelector(`[name=cardholder-name]`).value;
 }
